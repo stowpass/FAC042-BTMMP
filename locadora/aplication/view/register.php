c3f34aa8d014498a3871ddc7ca2b7026ff570360
Breno Teixeira <inf.teixeira@gmail.com>
2018-10-30 00:37:44 -0300

    Tentiva de conexão CADASTRAR

--- locadora/aplication/view/register.php
+++ locadora/aplication/view/register.php
@@ -1,3 +1,76 @@
+<?php
+session_start();
+ 
+/**
+ * Include ircmaxell's password_compat library.
+ */
+require '../model/password.php';
+ 
+/**
+ * Include our MySQL connection.
+ */
+require '../model/connect.php';
+ 
+ 
+//If the POST var "register" exists (our submit button), then we can
+//assume that the user has submitted the registration form.
+if(isset($_POST['register'])){
+    
+    //Retrieve the field values from our registration form.
+    $username = !empty($_POST['email']) ? trim($_POST['email']) : null;
+    $pass = !empty($_POST['senha']) ? trim($_POST['senha']) : null;
+    
+    //TO ADD: Error checking (username characters, password length, etc).
+    //Basically, you will need to add your own error checking BEFORE
+    //the prepared statement is built and executed.
+    
+    //Now, we need to check if the supplied username already exists.
+    
+    //Construct the SQL statement and prepare it.
+    $sql = "SELECT COUNT(email) AS num FROM tbl_usuario WHERE email = :email";
+    $stmt = $pdo->prepare($sql);
+    
+    //Bind the provided username to our prepared statement.
+    $stmt->bindValue(':email', $username);
+    
+    //Execute.
+    $stmt->execute();
+    
+    //Fetch the row.
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    
+    //If the provided username already exists - display error.
+    //TO ADD - Your own method of handling this error. For example purposes,
+    //I'm just going to kill the script completely, as error handling is outside
+    //the scope of this tutorial.
+    if($row['num'] > 0){
+        die('That username already exists!');
+    }
+    
+    //Hash the password as we do NOT want to store our passwords in plain text.
+    $passwordHash = password_hash($pass, PASSWORD_BCRYPT, array("cost" => 12));
+    
+    //Prepare our INSERT statement.
+    //Remember: We are inserting a new row into our users table.
+    $sql = "INSERT INTO tbl_usuario (email, senha) VALUES (:email, :senha)";
+    $stmt = $pdo->prepare($sql);
+    
+    //Bind our variables.
+    $stmt->bindValue(':email', $username);
+    $stmt->bindValue(':senha', $passwordHash);
+ 
+    //Execute the statement and insert the new account.
+    $result = $stmt->execute();
+    
+    //If the signup process is successful.
+    if($result){
+        //What you do here is up to you!
+        echo 'Thank you for registering with our website.';
+    }
+    
+}
+ 
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -36,7 +109,7 @@
   <div class="register-box-body">
     <p class="login-box-msg">Registre-se para utilizar de nossos serviços</p>
 
-    <form action="#" method="post">
+    <form action="register.php" method="post">
       <div class="form-group has-feedback">
         <input type="text" class="form-control" placeholder="Nome Completo" id="nome" name="nome">
         <span class="glyphicon glyphicon-user form-control-feedback"></span>
